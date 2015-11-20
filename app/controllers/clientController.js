ef507854d91ff11f10c3eae962c05535e91d9edb
Jonathan Coleman <jcoleman3307@gmail.com>
2015-11-20 12:42:29 +0000

    modify clientController URL

--- app/controllers/clientController.js
+++ app/controllers/clientController.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-   var appUrl = window.location.origin;    
-   var apiUrl = window.location.origin + 'api/getUser';
+   var appUrl = window.location.origin + '/';    
+   var apiUrl = appUrl + 'api/getUser';
    
 //get user and load search results from saved search
    $.get(apiUrl, function(data){
